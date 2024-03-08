69fa90997644c91da9c60d68694e62caeff3b6b0
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-03-09 00:37:51 +0800

    Profile Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,16 @@
+import { Profile } from './Profile/Profile';
+import user from '../data/user.json';
+
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </>
+  );
 };
