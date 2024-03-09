cd9117cbf38b277103b782deb584c368ea94e242
lesjohnpaul <55284022+lesjohnpaul@users.noreply.github.com>
2024-03-09 23:05:30 +0800

    added style to profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
+import React from 'react';
 import { Profile } from './Profile/Profile';
 import user from '../data/user.json';
+import avatar from '../images/lespaul.png';
 
 export const App = () => {
   return (
@@ -8,7 +10,7 @@ export const App = () => {
         username={user.username}
         tag={user.tag}
         location={user.location}
-        avatar={user.avatar}
+        avatar={avatar}
         stats={user.stats}
       />
     </>
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+import css from './FriendList.module.css';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.FriendList}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+};
